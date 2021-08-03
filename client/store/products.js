e00bf248f6559d78e67520f29b21517711ee201f
Margareth Borba <mborba1@icloud.com>
2021-08-03 11:56:22 -0400

    product store

---
+++ client/store/products.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const  SET_PRODUCTS = 'SET_PRODUCTS';
+
+const setProducts = (products) => ({
+    type: SET_PRODUCTS, products
+})
+
+
+export const fetchProducts = () => async dispatch => {
+    try{
+      const { data } = await axios.get('/products')
+      dispatch(setProducts(data))
+    }catch (err){
+      console.log(err)
+    }
+}
+
+export default function productsReducer(state=[], action){
+    switch (action.type){
+        case SET_PRODUCTS:
+            return action.products
+        default:
+            return state    
+    }
+}
