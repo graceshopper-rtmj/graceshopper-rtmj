ee37491aad01bf0aa08a5c5f7f1f9d77aaabc0fa
Cristina Satterfield <cristi.satterfield@gmail.com>
2021-08-04 08:17:48 -0700

    Task: Create association between User and Product models

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,12 @@
 //this is the access point for all things database related!
 
 const db = require('./db')
-const Product = require ('./models/Products')
+const Product = require('./models/Products')
 const User = require('./models/User')
 
 //associations could go here!
+Product.belongsToMany(User, { through: "Cart" });
+User.belongsToMany(Product, { through: "Cart" });
 
 module.exports = {
   db,
