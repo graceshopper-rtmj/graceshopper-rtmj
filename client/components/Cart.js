fcc09480eb53f75756ca797d0e597d53ff64331f
Rachel Kahtan <kahtan.rachel@gmail.com>
2021-08-04 15:51:36 -0400

    button: delete from cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -3,16 +3,17 @@ import { connect } from 'react-redux';
 
 class Cart extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       cart: [],
       error: null,
       loading: true,
-    }
+    };
+    this.handleDelete = this.handleDelete.bind(this)
   }
   componentDidMount() {
     try {
-      this.setState({cart: JSON.parse(localStorage.cart)})
+      this.setState({ cart: JSON.parse(localStorage.cart) });
     } catch (err) {
       this.setState({ error: err.message, loading: true });
     }
@@ -24,43 +25,53 @@ class Cart extends React.Component {
       this.setState({ loading: false });
     }
   }
- 
+  handleDelete(e) {
+    let idx = e.target.value
+    let cart = this.state.cart
+    let left = cart.slice(0, idx)
+    let right = cart.slice(idx+1)
+    cart = [...left, ...right]
+    localStorage.cart = JSON.stringify(cart);
+    this.setState({cart: JSON.parse(localStorage.cart)})
+  }
   render() {
     return (
       <div>
         <h1>YOUR CART:</h1>
-        <div style={{border: "3px black solid"}}>
-          {!this.state.cart.length 
-          ? <h2>There are no items in your cart!</h2>
-          :
-          (this.state.cart.map(item => {
-            return (
-              <div key={item.productId} style={{border: "1px black solid"}}>
-              <h3>{item.product.name}</h3>
-              <h3>${item.product.price * item.qty}</h3>
-              <h3>There are {item.qty} in your cart</h3>
-              </div>
-            )
-          }))
-        }
-
+        <div style={{ border: '3px black solid' }}>
+          {!this.state.cart.length ? (
+            <h2>There are no items in your cart!</h2>
+          ) : (
+            this.state.cart.map((item, idx) => {
+              return (
+                <div key={item.productId} style={{ border: '1px black solid' }}>
+                  <h3>{item.product.name}</h3>
+                  <h3>${item.product.price * item.qty}</h3>
+                  <h3>There are {item.qty} in your cart</h3>
+                  <button
+                    value={idx}
+                    type='button'
+                    onClick={this.handleDelete}
+                  >
+                    Remove from cart
+                  </button>
+                </div>
+              );
+            })
+          )}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default connect()(Cart)
-
-
-
+export default connect()(Cart);
+//probably need it connected for logged in user later!
 
 //update cart buttons?
 //increase/decrease qty
 //can't increase more than the item's quantity
 //delete item
-
-//notify if empty cart
 
 //purchase button
 //clear local storage
