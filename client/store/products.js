598b4f06ce4e0074528b46b425af6297773ba431
Cristina Satterfield <cristi.satterfield@gmail.com>
2021-08-04 08:37:24 -0700

    Task: Create backend for adding to cart

--- client/store/products.js
+++ client/store/products.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
 const SET_PRODUCTS = 'SET_PRODUCTS';
+const ADD_PRODUCT_TO_CART = "ADD_PRODUCT_TO_CART"
 
 const setProducts = (products) => ({
   type: SET_PRODUCTS,
   products,
 });
+
+const _addProductToCart = (product) => ({
+  type: ADD_PRODUCT_TO_CART,
+  product
+})
 
 
 export const fetchProducts = () => async (dispatch) => {
@@ -17,12 +23,24 @@ export const fetchProducts = () => async (dispatch) => {
   }
 };
 
+export const addProductToCart = (productId) => async (dispatch) => {
+  try {
+    const { data } = await axios.put(`/api/${productId}/add-to-cart`);
+    dispatch(_addProductToCart(data))
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 
 export default function productsReducer(state = [], action) {
   switch (action.type) {
     case SET_PRODUCTS:
       return action.products;
+    case ADD_PRODUCT_TO_CART:
+      return action.product;
     default:
       return state;
   }
 }
+
