763dba9f1c8d2e5d02b67b7cf843dbc6a5cd6915
Cristina Satterfield <cristi.satterfield@gmail.com>
2021-08-09 11:56:19 -0700

    Task: Protect GET /auth/me route using gatekeeping Middleware

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
-const { models: { User, Sale, Product } } = require('../db')
+const { models: { User, Sale, Product } } = require('../db');
+const { requireToken } = require('../api/gatekeepingMiddleware');
 module.exports = router
 
 router.post('/login', async (req, res, next) => {
@@ -25,9 +26,10 @@ router.post('/signup', async (req, res, next) => {
   }
 })
 
-router.get('/me', async (req, res, next) => {
+// GET /auth/me
+router.get('/me', requireToken, async (req, res, next) => {
   try {
-    const user = await User.findByToken(req.headers.authorization)
+    const user = req.user;
     const cart = await Sale.findOne({
       where: {
         userId: user.id,
