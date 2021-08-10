9fc24821388741f9ee9c4523093c98c0643cabed
Josephine <80869118+joseewang@users.noreply.github.com>
2021-08-10 09:41:08 -0400

    buttons added, Cart info rendering, cant log in

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,39 +1,101 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+import Cart from "./Cart";
+
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 class Checkout extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      username: '',
+      username: "",
+      open: false,
     };
+
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
-  render(){
+
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
     console.log("checkout page this.props", this.props);
-    const {username} = this.state;
+    console.log('checkout page localstorage:', JSON.parse(localStorage.cart));
+    console.log('Cart component:', Cart);
+    const { username, open } = this.state;
+    const { handleClickOpen, handleClose } = this;
 
     return (
       <div>
         <div>
           <Link to={"/login"}>
-            <button>Sign In</button>
+            <Button variant="outlined">Sign In</Button>
           </Link>
-          <Link to={"/signup"}>
+          {/* <Link to={"/signup"}>
             <button>Create Account</button>
-          </Link>
+          </Link> */}
         </div>
-        <form>
-          <label>Enter email address:</label>
-          <input username="email address" onChange={this.handleChange} value={username} />
-          <button type="submit">Checkout as guest</button>
-        </form>
+        <div>
+          <Button onClick={handleClickOpen} variant="outlined">Checkout as guest</Button>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title">Sign Up</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Please enter your email address to sign up for an account.
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Email Address"
+                type="email"
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="new-password"
+                label="Password"
+                type="password"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button type="submit" onClick={handleClose} color="primary">
+                Sign Up
+              </Button>
+              <Button onClick={handleClose} color="primary">Cancel</Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+        <div>
+          <Cart>My Cart:</Cart>
+        </div>
         <Link to={"/cart/confirmation"}>
-          <button>Place Order</button>
+          <Button variant="outlined">Place Order</Button>
         </Link>
       </div>
-    )
+    );
   }
 }
 
